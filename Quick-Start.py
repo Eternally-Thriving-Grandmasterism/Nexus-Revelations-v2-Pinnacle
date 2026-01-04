813d8873f328634e9f0af851d97a852406d3da9d
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-04 18:16:44 -0500

    Create Quick-Start.py

---
+++ Quick-Start.py
@@ -0,0 +1,5 @@
+from nexus_revelations_v2 import RevelationStreamer
+
+streamer = RevelationStreamer()
+insights = streamer.inject_insights(query="cosmic harmony")
+streamer.stream_revelations(scope="eternal")
